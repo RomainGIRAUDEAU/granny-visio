eb958af6c5d04b6e0c57331fc338f27d1835cc6e
Romain <romain.giraudeau639@gmail.com>
2020-11-07 18:06:23 +0100

    add app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./clientSide/index.html', function (err, html) {
+fs.readFile('./clientSide/index.html',  function (err, html) {
     if (err) {
         throw err;
     }
